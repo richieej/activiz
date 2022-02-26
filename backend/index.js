d8bcbb50060e889e8bf3f04cdb4d4db2f663b5f1
Sophia Adrangi <sophiaadrangi@gmail.com>
2022-02-26 14:24:50 -0800

    mongoose, cors, and express installed in backend. database connected

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,32 @@
-console.log("backend working")
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+const app = express();
+
+
+
+
+
+//json data cannot be greater than 20mb
+//extended: true allows parsing of non-strings as well 
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+//app.use('/posts', postRoutes)
+//ever route inside of postRoutes will start with localhost:5000/posts 
+
+//mongodb atlas
+
+
+const CONNECTION_URL = "mongodb+srv://activiz:foundmyiphone2022@cluster.ey7xf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const PORT = process.env.port || 5000
+
+
+//PLEASE USE TEST_URL WHEN YOU'RE TESTING INSTEAD OF CONNECTION_URL
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    
+}).then( () => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch( (error) => console.log(error.message))
